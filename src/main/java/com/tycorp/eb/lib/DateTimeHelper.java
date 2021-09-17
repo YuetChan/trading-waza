74055d92754e251f1aef3ed5d2b1868e0e54ed72
Yuet Chan <yuetcheukchan@gmail.com>
2021-09-16 22:28:48 -0400

    Added DateTimeHelper and truncate processedAt in postsGetByFilter

---
+++ src/main/java/com/tycorp/eb/lib/DateTimeHelper.java
@@ -0,0 +1,17 @@
+package com.tycorp.eb.lib;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class DateTimeHelper {
+
+    public static Long zonedDateTimeToEpoch(ZonedDateTime zdt) {
+        return zdt.toInstant().toEpochMilli();
+    }
+
+    public static ZonedDateTime truncateTime(ZonedDateTime zdt) {
+        ZoneId zoneId  = zdt.getZone();
+        return zdt.toLocalDate().atStartOfDay(zoneId);
+    }
+
+}
